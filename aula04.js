a0059ea21741fede331c57523bdb3d48c7c2c2a7
silvanamedeiros <silvanamedeiros67@yahoo.com>
2025-05-08 21:39:47 -0300

    Encerrando o Javascript por hj

---
+++ aula04.js
@@ -0,0 +1,17 @@
+//veja abaixo exemplo de como e qdo usar as crases
+let nome = 'Silk'; // criamos uma variável tipo String para guardar o nome do usuário
+let idade = 53; //Número usado sem aspas (simples ou duplas), //Criamos uma variável tipo number para guardar a idde do usuário
+
+let mensagem = `Olá, meu nome é ${nome} e tenho ${idade} anos`; // Criamos uma variável chamada mensagem para exibir o texto contendo as duas variáveis que foram declaradas
+
+
+console.log(mensagem); // Criamos o console.log para fazer a impressão da variável "mensagem".
+
+//criem uma variável de profissão e salário
+var profissao = 'desenvolvedor de web';
+var salario = 5000
+var retornoSalario = `A profissão dele é ${profissao} e ele recebe ao mês ${salario} reais`;
+console.log(retornoSalario);
+
+// criem uma variável para informar raça e idade de um cachorro
+
---
+++ aula05.js
@@ -0,0 +1,6 @@
+let primeiroNome = 'John';
+let ultimoNome = 'Doe';
+let nomeCompleto = primeiroNome + ' ' + ultimoNome; // Usando o +
+let nomeCompletoTemplate = `${primeiroNome} ${ultimoNome}`; // Using template literal
+
+console.log(nomeCompletoTemplate);
