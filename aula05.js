d4cd32a309065050806165708e3e6e2fff133b54
Lisboa Gabriel <silvanamedeiros67@yahoo.com>
2025-05-09 21:35:10 -0300

    treinando Javascript

--- aula05.js
+++ aula05.js
@@ -1,6 +1,37 @@
+//BLOCO 01
 let primeiroNome = 'John';
 let ultimoNome = 'Doe';
 let nomeCompleto = primeiroNome + ' ' + ultimoNome; // Usando o +
 let nomeCompletoTemplate = `${primeiroNome} ${ultimoNome}`; // Using template literal
 
-console.log(nomeCompletoTemplate);
+//console.log(nomeCompletoTemplate);
+
+//criem 3 blocos de variáveis qu contenham 1 - (Países e Capitais); 2 - ( Tipos de Dados Bancários); 3 - (Renda Familiar)
+
+//Tarefa (1 - Países e Capitais)
+let ch = 'China';
+let peq = 'Pequim';
+let jp = 'japão';
+let tq = 'Tóquio';
+let cs = 'Coréia do Sul';
+let sl = 'Seul';
+let paisesOrientais = `${ch}, ${jp} e ${cs} são países Orientais, suas Capitais são respectivamente, ${peq}, ${tq} e ${sl}.`
+
+console.log(paisesOrientais);
+
+//Tarefa(2 - Dados Bancários)
+let cod = 'Código do Banco:';
+let nm = 260 ;
+let ag = 'Agência:';
+let nu = 0001;
+let tc = 'Tipo de Conta:';
+let cp = 'Conta Poupança';
+let nr = 'Numero da Conta:';
+let no = '012345-43'
+let dadosBancarios = `Para acessarmos sua conta precisamos das seguintes informações, ${cod} ${nm}, ${ag} ${nu}, ${tc} ${cp} e ${nr} ${no}.`
+
+console.log(dadosBancarios);
+
+//Tarefa(3 - Renda Familiar)
+let 
+
