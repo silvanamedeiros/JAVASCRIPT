5e2766ff1d0aa302aa26368a91ed6f1dc64195b0
silvanamedeiros <silvanamedeiros67@yahoo.com>
2025-05-13 21:47:16 -0300

    logica

---
+++ aula08.js
@@ -0,0 +1,29 @@
+let x = 10;
+let y = '10';
+
+console.log(x == y);
+ 
+console.log(x === y);
+// estamos testando se o vlor de X é igual ao valor de Y
+// as respostas serão true ( para verdeiro/ afirmação) false ( para falso/ negação)
+
+
+let frutas ='banana';
+let frutas2 = 'maçã';
+// aqui estamos perguntando se o valor da variável "frutas" é diferente da variável "frutas2"
+console.log(frutas !-- frutas2);
+
+let numero = 20;
+let segundoNumero = 258;
+//estamos utilizando (maior que >) e (menor que <) para comparar determinadas informações
+
+console.log(numero > 30);  
+//(>) maior que...
+//(<) menor que...
+
+let numero = 10;
+let segundoNumero = 258;
+// estamos utilizando 
+
+
+
