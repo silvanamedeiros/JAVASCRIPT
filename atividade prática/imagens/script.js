68d068a8a84ce03159c9d0fc0b7cf247cb47a1f3
silvanamedeiros <silvanamedeiros67@yahoo.com>
2025-05-22 21:23:17 -0300

    aula de js

--- atividade prática/imagens/script.js
+++ atividade prática/imagens/script.js
@@ -8,13 +8,13 @@ function changeImage() {
     const currentSrc = img.getAttribute("src");//aqui pegamos o atributo SRC da imagem, reponsãvel pela alteração da img.
 
     if (currentSrc === "imagem01.png") { //Se (if) a origem atual da imagem for igual a "imagem01.png"
-        img.setAttribute("src", "imagem01.jpg") //Usando o setAttribute vamos modificar o valor do src, especificando a outra img
-        img.setAttribute("alt", "imagem02.jpg");
+        img.setAttribute("src", "imagem02.jpg") //Usando o setAttribute vamos modificar o valor do src, especificando a outra img
+        img.setAttribute("alt", "mesa na varanda");
 
     }else{
         //Se não, fazer o processo inverso basicamente
-        img.setAttribute("src", "imagem02.jpg");
-        img.setAttribute("alt", "imagem01.jpg");
+        img.setAttribute("src", "imagem01.jpg");
+        img.setAttribute("alt", "homem na varanda");
     }
 }
 //if = se a condição for válida execute algo
